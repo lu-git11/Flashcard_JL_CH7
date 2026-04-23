80d27d52dc637aa11ba320da3d6e0c936833bed7
Jeffrey Lullen <lullenj@gmail.com>
2026-04-23 08:33:02 +0800

    Initial Commit

---
+++ Flashcard_JL_CH7/Flashcard_JL_CH7App.swift
@@ -0,0 +1,17 @@
+//
+//  Flashcard_JL_CH7App.swift
+//  Flashcard_JL_CH7
+//
+//  Created by jeffrey lullen on 4/23/26.
+//
+
+import SwiftUI
+
+@main
+struct Flashcard_JL_CH7App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
